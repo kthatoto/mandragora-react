da85b65649abb4d8f37f061776f092cd5967c93e
tk1to <tk02dr@gmail.com>
2017-08-04 02:27:53 +0900

    canvas追加

---
+++ js/canvas.js
@@ -0,0 +1,9 @@
+
+$(function(){
+  // canvasの大きさを指定
+  var h = $("#graph").height();
+  var w = $("#graph").width();
+  var canvas = $("#graph_canvas");
+  canvas.attr("height", h);
+  canvas.attr("width",  w);
+});
