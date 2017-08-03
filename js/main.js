5fc9dff6f3127d6f8d4e6eab63bccee84d0a77e4
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-08-03 17:53:27 +0900

    音を鳴らすようにした。

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// AudioElement を作成
+var audio;
+
+window.onload = function () {
+  // ここに読み込み完了時に実行してほしい内容を書く。
+  audio = new Audio();
+  // プリロードを設定する
+  audio.preload = "none";
+  // サウンドファイルまでの URL アドレスを指定
+  audio.src = "audio/uni1464.wav";
+  // 読み込みを開始する
+  audio.load();
+};
+
+function weiwei(){
+  audio.play();
+}
+
+
